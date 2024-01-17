d03ceb1c2d3ebc5feaf66b9139c69560eec093be
Angel Borrego Marron <angelbormar16@gmail.com>
2024-01-17 19:28:04 +0100

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@
 function Footer() {
     return (
         <footer>
-            <div>  <a href="https://github.com/gerard02000"> &copy; gerard02000 </a> </div>
             <div>  2000 </div>
         </footer>
     )
